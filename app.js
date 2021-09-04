c1944a29085eee2fd2497a7cbfc07c00ea6ba585
Oz Volmar <o.volmar@gmail.com>
2021-09-04 11:11:49 -0400

    changed message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! Lets get this project going'));
+app.get('/', (req, res) => res.send('Lets get this project going'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
